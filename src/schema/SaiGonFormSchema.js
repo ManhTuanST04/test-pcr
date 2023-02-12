a047681941313275fd4eca417a915cf0860c9488
Tuấn Vũ <manhtuanst04@gmail.com>
2023-02-12 22:21:11 +0700

    fix bug sai gon

--- src/schema/SaiGonFormSchema.js
+++ src/schema/SaiGonFormSchema.js
@@ -4,7 +4,7 @@ const SaiGonFormSchema = object().shape({
     patientName: string().required('Bắt buộc nhập'),
     address: string().required('Bắt buộc nhập'),
     passport: string().required('Bắt buộc nhập'),
-    sex: string().required('Bắt buộc nhập').default('Nam (Male)'),
+    sex: string().required('Bắt buộc nhập').default('Nam'),
     dateOfBirth: string().required('Bắt buộc nhập').default('01/01/1990').nullable(),
     nationality: string().required('Bắt buộc nhập').default('Việt Nam'),
     samplingTime: string().required('Bắt buộc nhập').default('').nullable(),
